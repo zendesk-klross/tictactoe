c2d751363b94aca604a2e4e8ac6f2306340c509f
Katie Louise Ross <katielouise.ross@zendesk.com>
2024-03-01 12:30:29 +1100

    fix: apply checks to the board's grid attribute

--- tictactoe/board.py
+++ tictactoe/board.py
@@ -4,26 +4,26 @@ from typing import Optional
 
 class Board:
 
-    # @TODO: Write a setter for the board cell values
     def __init__(self, col:int=3, row:int=3):
         self.col = col
         self.row = row
-        self.board = np.array([str(i) for i in range(1, self.col*self.row + 1)]).reshape(self.col, self.row)
+        self.grid = np.array([str(i) for i in range(1, self.col*self.row + 1)]).reshape(self.col, self.row)
+  
     def __repr__(self):
-        return str(self.board)
+        return str(self.grid)
 
     def check_winner(self) -> Optional[str]:
         # Check rows and columns for a winner
-        for i in self.shape[0]:
-            if np.all(self[i, :] == "X") or np.all(self[:, i] == "X"):
+        for i in range(self.grid.shape[0]):
+            if np.all(self[i, :] == "X") or np.all(self.grid[:, i] == "X"):
                 return "X wins!"
-            elif np.all(self[i, :] == "O") or np.all(self[:, i] == "O"):
+            elif np.all(self.grid[i, :] == "O") or np.all(self.grid[:, i] == "O"):
                 return "O wins!"
 
         # Check diagonals for a winner
-        if np.all(np.diag(self) == "X") or np.all(np.diag(np.fliplr(self)) == "X"):
+        if np.all(np.diag(self.grid) == "X") or np.all(np.diag(np.fliplr(self.grid)) == "X"):
             return 'X wins!'
-        elif np.all(np.diag(self) == "O") or np.all(np.diag(np.fliplr(self)) == "O"):
+        if np.all(np.diag(self.grid) == "O") or np.all(np.diag(np.fliplr(self.grid)) == "O"):
             return "O wins!"
 
         # Check for a tie (no numbers left, meaning all cells are filled with 'X' or 'O')
