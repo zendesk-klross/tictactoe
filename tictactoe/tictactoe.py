40cf72c1c029620339de794d02865613d91441d5
Katie Louise Ross <katielouise.ross@zendesk.com>
2024-03-04 15:52:57 +1100

    feat: add winner check to game script

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -14,17 +14,22 @@ class TicTacToe:
     def play(self):
         print("TicTacToe <3")
         print(self.board.__repr__())
-        while True:
-            # self.board.check_winner()
+        while self.is_played:
             current_player = self.player2 if self.turn else self.player1
-            print("Turn: {}".format(current_player.name))
-            move = input("Your move: ")
-            move_position = np.where(self.board.grid == move)
-            self.board.make_move(move_position, current_player.token)
-            self.turn = 0 if self.turn else 1
-            print ("Moved to {}".format(move_position))
-            print ("=============================")
             print(self.board.__str__())
+            end_game = self.board.check_winner()
+            if end_game:
+                print(end_game)
+                self.is_played = False
+            
+            else:
+                print("Turn: {}".format(current_player.name))
+                move = input("Your move: ")
+                move_position = np.where(self.board.grid == move)
+                self.board.make_move(move_position, current_player.token)
+                self.turn = 0 if self.turn else 1
+                print ("Moved to {}".format(move_position))
+                print ("=============================")
 
 board = Board()
 player1 = Player("X", "Player1")
