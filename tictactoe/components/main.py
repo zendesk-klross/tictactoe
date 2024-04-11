ddc6b11ed7c55b553766e53e2455068712869b54
Ana Lastoviria <ana.lastoviria@zendesk.com>
2024-04-11 17:24:17 +1000

    wip - playing with form input

--- tictactoe/components/main.py
+++ tictactoe/components/main.py
@@ -1,11 +1,11 @@
-from reactpy import component, html, run
+from reactpy import component, html, run, hooks
 
 def cell_style():
     return {
         "style": {
             "width": "3em",
             "height": "3em",
-            "border": "5px solid red",
+            "border": "1px dotted black",
         },
     }
 
@@ -16,6 +16,12 @@ def table_style():
         },
     }
 
+def form_style():
+    return {
+        "style": {
+            "margin-top": "1em",
+        },
+    }
 
 @component
 def Main():
@@ -25,7 +31,7 @@ def Main():
     return (html.div(
         html.h1("XoXoXo"),
         Grid(GRID_HEIGHT, GRID_WIDTH, "*"),
-        InputBox
+        InputBox()
     ))
 
 
@@ -44,8 +50,42 @@ def Grid(height, width, filler):
 
     return render_grid(height, width, filler)
 
+@component
+def InputBox():
+
+    input_val, set_input_val = hooks.use_state('')
+    form_state, set_form_state = hooks.use_state('')
+
+    def on_change(event):
+        set_input_val(event['target']['value'])
+
+    def handle_submit(event):
+        set_form_state("Form submitted")
+
+    return html.div(
+        html.form(
+        {"on_submit": handle_submit},
+        html.label({"htmlFor": "textField"}, "Make your move: "),
+        html.input({
+            "type": "text",
+            "id": "textField",
+            "name": "textField",
+            "value": input_val,
+            "onChange": on_change,
+        }),
+        html.input({
+            "type": "submit"
+        }),),
+        html.p(input_val),
+        html.p(form_state)
+    )
 
 run(Main)
 
 # TODO:
 # - can components inherit props? (for the value)
+
+#  type="text"
+#         value={inputValue}
+#         onChange={(e) => setInputValue(e.target.value)}
+#         placeholder="Enter something..."
