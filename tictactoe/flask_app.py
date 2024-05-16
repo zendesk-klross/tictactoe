6b30422e409b5e174752f870fa837477dd2a747a
Katie Louise Ross <katielouise.ross@zendesk.com>
2024-05-16 11:10:34 +1000

    create set up page for user inputs

--- tictactoe/flask_app.py
+++ tictactoe/flask_app.py
@@ -14,5 +14,23 @@ app.secret_key = os.urandom(24)  # Needed to use sessions
 def index():
     return render_template('index.html')
 
+@app.route('/setup', methods=['GET', 'POST'])
+def setup():
+    if request.method == 'POST':
+        player1_type = request.form['player1_type']
+        player2_type = request.form['player2_type']
+        board_size = int(request.form['board_size'])
+
+        player1 = Player(name='Player 1', token='X', human=player1_type == 'human')
+        player2 = Player(name= 'Player 2', token='O', human=player2_type == 'human')
+        board = Board(col=board_size, row=board_size)
+        game = TicTacToe(board, player1, player2)
+
+        session['game_data'] = game.to_dict()
+        return redirect(url_for('play'))
+    
+    return render_template('setup.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
